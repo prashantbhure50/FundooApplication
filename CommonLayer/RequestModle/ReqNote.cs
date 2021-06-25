0150a5dd9dfe712adf08917a19c996bb7132025a
Prashantbhure50 <prashantbhure50@gmail.com>
2021-06-25 15:35:45 +0530

    [Prashant] ReqNote Is Created

---
+++ CommonLayer/RequestModle/ReqNote.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommonLayer.RequestModle
+{
+   public class ReqNote
+    {
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public string Reminder { get; set; }
+        public string Color { get; set; }
+        public string Archive { get; set; }
+        public string Trash { get; set; }
+        public string Pin { get; set; }
+        public int UserId { get; set; }
+    }
+}
